64206127bb2a272e787e1798a0dc56c7d661aa8c
Edward <earodriguez154@misena.edu.co>
2021-02-25 16:26:47 -0500

    Ada comit

---
+++ AdaPrueba/pruebaConocimineto/Punto2/CarritoCompras2/CarritoCompras/Categoria.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using ComponenteNegocio;
+using ComponenteDatos;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CarritoCompras
+{
+    public partial class Contact : Page
+    {
+        DataTable dtb;
+        DataTable carrito = new DataTable();
+        string html = "";
+
+        public void CargarDetalle()
+        {
+            
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+                
+            
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
